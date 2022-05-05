2dd9a77c6d3c772d979bd876e961fe27ecba9c80
Atharv <Deshpandes895@gmail.com>
2022-05-05 15:03:32 +0530

    SpringBoot First Project

---
+++ src/main/java/springbootstart/springapp/SpringApp.java
@@ -0,0 +1,18 @@
+package springbootstart.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp {
+    public static void main(String[] args)
+    {
+        try{
+            SpringApplication.run(SpringApp.class,args);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
---
+++ src/main/java/springbootstart/springapp/hello/HelloController.java
@@ -0,0 +1,13 @@
+package springbootstart.springapp.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String sayhi()
+    {
+        return "Hii My Name Is Atharv";
+    }
+}
